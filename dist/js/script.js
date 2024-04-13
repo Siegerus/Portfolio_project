72d838b5cb05432c7089d870cf75b722fb981d96
Siegerus <ivanoffsiegerus@gmail.com>
2024-04-13 16:42:57 +0300

    end

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,3 +15,20 @@ close.addEventListener ('click', () => {
     menu.classList.remove('active');
 }) 
 
+// "document.querySelectorAll" - обращяемся уо всем селекторам с таким именем на странице.
+const percent = document.querySelectorAll('.skills__scale-percent') // 
+      bodys = document.querySelectorAll('.skills__scale-body span') // константа всех спенов внутри "skills__scale-body" , которые будут жёлтыми.
+
+//console.log(scales); // можно вывести в лог значение константы (в данном случае это массив со всеми '.skills__scale-percent'). 
+
+// ".forEach" - Для каждого ".skills__scale-percent" (перебираем массив). "item" - уже конкретный элемент массива, для него
+// будет функция. "(item, i) bodys[i]" говорим что элемент будет браться по индексу (каждый '.skills__scale-percent' будет соответствовать 
+// своему '.skills__scale-body span'). ".style.width" - тоже самое что и "style="width: 50%;"".
+//"item.innerHTML;" - Вытаскиваем значение "item"('.skills__scale-percent') из документа.
+//Таким образом кладём значение наших "percent" в "bodys". В итоге ширина наших '.skills__scale-body span' будет такоой же, 
+//какое значение записано в '.skills__scale-percent'.
+    percent.forEach( (item, i) => {
+        bodys[i].style.width = item.innerHTML;
+    });
+
+    
